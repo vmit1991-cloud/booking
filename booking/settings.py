12058031b4c166cfa8378b7631778f17d89e3007
moskovchuk <moskovchuk@moskovchuk>
2026-02-24 14:22:42 +0200

    update allow host

--- booking/settings.py
+++ booking/settings.py
@@ -20,9 +20,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    ".onrender.com",
+    "*",
 ]
 
 
